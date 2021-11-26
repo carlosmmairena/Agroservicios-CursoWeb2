933e44f13f5763174aa6dc8835a7f79cddbbb61b
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-11-26 12:58:55 -0600

    Crud de productos de contruccion completo

--- api/src/controller/ConstruccionProductoController.ts
+++ api/src/controller/ConstruccionProductoController.ts
@@ -2,16 +2,16 @@ import { getRepository } from "typeorm";
 import { Request, Response } from "express";
 import { Producto } from "../entity/Producto";
 import { isNull, isUndefined } from "util";
-import { Insumo } from "../entity/Insumo";
+import { Construccion } from "../entity/Construccion";
 
-export class InsumoProductoController {
+export class ConstruccionProductoController {
 
-    static allInsumo = async (request: Request, response: Response) => {
-        const productRepository = getRepository(Insumo);
+    static allConstruccion = async (request: Request, response: Response) => {
+        const productRepository = getRepository(Construccion);
         const products = await productRepository.find({ relations: ['producto'] });
         
         if (products.length < 1) {
-            return response.status(404).json({ message: 'No hay productos de insumo registrados.' });
+            return response.status(404).json({ message: 'No hay productos de construcción registrados.' });
         }
 
         return response.status(200).json(products);
@@ -19,18 +19,18 @@ export class InsumoProductoController {
 
 
     /**
-     * Guarda un producto de tipo Insumo.
+     * Guarda un producto de tipo Construccion.
      * 
      * @param request 
      * @param response 
      * @returns status
      */
-    static saveInsumo = async (request: Request, response: Response) => {
+    static saveConstruccion = async (request: Request, response: Response) => {
         try {
             const productRepository = getRepository(Producto);
-            const { nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fechaVencimiento, tipoInsumo } = request.body;
+            const { nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fragil } = request.body;
             
-            const dataValidated = Insumo.checkData({ nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fechaVencimiento, tipoInsumo });
+            const dataValidated = Construccion.checkData({ nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fragil });
             
             if (dataValidated.hasErrors) {
                 return response.status(422).json({ message: "Los siguientes campos están mal", errors: dataValidated.errors });
@@ -52,24 +52,23 @@ export class InsumoProductoController {
             productToSave.unidadMedida   = unidadMedida;
             productToSave.estado         = estado;
 
-            const insumoToSave = new Insumo();
+            const construccionToSave = new Construccion();
 
-            insumoToSave.fechaVencimiento = fechaVencimiento;
-            insumoToSave.tipoInsumo       = tipoInsumo;
-            insumoToSave.producto         = productToSave;
+            construccionToSave.fragil   = fragil;
+            construccionToSave.producto = productToSave;
 
-            const formatoDatosInsumoValidado = await Insumo.validate(insumoToSave);
+            const formatoDatosConstruccionValidado = await Construccion.validate(construccionToSave);
 
-            if (formatoDatosInsumoValidado.length) {
-                return response.status(422).json({ message: "Los datos no cumplen con el formato adecuado", details: formatoDatosInsumoValidado });
+            if (formatoDatosConstruccionValidado.length) {
+                return response.status(422).json({ message: "Los datos no cumplen con el formato adecuado", details: formatoDatosConstruccionValidado });
             }
 
             await productRepository.save(productToSave);
-            const insumoProductRepository = getRepository(Insumo);
+            const construccionProductRepository = getRepository(Construccion);
 
-            const insumoProductSaved = await insumoProductRepository.save(insumoToSave);
+            const construccionProductSaved = await construccionProductRepository.save(construccionToSave);
             
-            return response.status(201).json({ message: 'Producto registrado', insumo: insumoProductSaved });
+            return response.status(201).json({ message: 'Producto registrado', cosntruccion: construccionProductSaved });
 
         } catch (error) {
             return response.status(503).json({ message: "Algo ha fallado...", errors: error });
@@ -78,12 +77,12 @@ export class InsumoProductoController {
 
 
     /**
-     * Modifica un producto de tipo Insumo
+     * Modifica un producto de tipo Construccion
      * @param request 
      * @param response 
      * @returns 
      */
-    static modifyInsumo = async (request: Request, response: Response) => {
+    static modifyConstruccion = async (request: Request, response: Response) => {
         try {
             const { id } = request.params;
             
@@ -93,16 +92,16 @@ export class InsumoProductoController {
 
 
             const productRepository = getRepository(Producto);
-            const { nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fechaVencimiento, tipoInsumo } = request.body;
+            const { nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fragil } = request.body;
             
-            const dataValidated = Insumo.checkData({ nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fechaVencimiento, tipoInsumo });
+            const dataValidated = Construccion.checkData({ nombre, descripcion, marca, precioUnitario, stock, unidadMedida, estado, fragil });
             
             if (dataValidated.hasErrors) {
                 return response.status(422).json({ message: "Los siguientes campos están mal", errors: dataValidated.errors });
             }
 
             // TODO: Debería haber un 'código de producto' para que el usuario pueda identificar de manera distinta un producto...
-            const productToEdit = await productRepository.findOne(id, { relations: ['insumo'] });
+            const productToEdit = await productRepository.findOne(id, { relations: ['construccion'] });
             if(!productToEdit) {
                 return response.status(404).json({ message: `No se encuentra el producto con ID: ${id}`});
             }
@@ -121,21 +120,20 @@ export class InsumoProductoController {
             productToEdit.unidadMedida   = unidadMedida;
             productToEdit.estado         = estado;
 
-            const insumoToEdit = productToEdit.insumo;
+            const insumoToEdit = productToEdit.construccion;
 
-            insumoToEdit.fechaVencimiento = fechaVencimiento;
-            insumoToEdit.tipoInsumo       = tipoInsumo;
-            insumoToEdit.producto         = productToEdit;
+            insumoToEdit.fragil   = fragil;
+            insumoToEdit.producto = productToEdit;
 
-            const formatoDatosInsumoValidado = await Insumo.validate(insumoToEdit);
+            const formatoDatosConstruccionValidado = await Construccion.validate(insumoToEdit);
 
-            if (formatoDatosInsumoValidado.length) {
-                return response.status(422).json({ message: "Los datos no cumplen con el formato adecuado", details: formatoDatosInsumoValidado });
+            if (formatoDatosConstruccionValidado.length) {
+                return response.status(422).json({ message: "Los datos no cumplen con el formato adecuado", details: formatoDatosConstruccionValidado });
             }
 
-            const insumoProductRepository = getRepository(Insumo);
+            const construccionProductRepository = getRepository(Construccion);
             await productRepository.save(productToEdit);
-            await insumoProductRepository.save(insumoToEdit);
+            await construccionProductRepository.save(insumoToEdit);
             
             return response.status(201).json({ message: 'Producto actualizado'});
 
--- api/src/routes/ProductRouter.ts
+++ api/src/routes/ProductRouter.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
+import { ConstruccionProductoController } from "../controller/ConstruccionProductoController";
 import { InsumoProductoController } from "../controller/InsumoProductoController";
 import { ProductoController } from "../controller/ProductoController";
 import { VeterinarioProductoController } from "../controller/VeterinarioProductoController";
 
 const PRODUCT_ROUTER = Router();
 
-PRODUCT_ROUTER.get('/veterinarios',       VeterinarioProductoController.allVeterinario);
-PRODUCT_ROUTER.post('/veterinario',       VeterinarioProductoController.saveVeterinario);
-PRODUCT_ROUTER.put('/veterinario/:id',    VeterinarioProductoController.modifyVeterinario);
+PRODUCT_ROUTER.get('/veterinarios',    VeterinarioProductoController.allVeterinario);
+PRODUCT_ROUTER.post('/veterinario',    VeterinarioProductoController.saveVeterinario);
+PRODUCT_ROUTER.put('/veterinario/:id', VeterinarioProductoController.modifyVeterinario);
 
-PRODUCT_ROUTER.get('/insumos',       InsumoProductoController.allVeterinario);
-PRODUCT_ROUTER.post('/insumo',       InsumoProductoController.saveInsumo);
-PRODUCT_ROUTER.put('/insumo/:id',    InsumoProductoController.modifyInsumo);
+PRODUCT_ROUTER.get('/insumos',    InsumoProductoController.allInsumo);
+PRODUCT_ROUTER.post('/insumo',    InsumoProductoController.saveInsumo);
+PRODUCT_ROUTER.put('/insumo/:id', InsumoProductoController.modifyInsumo);
+
+PRODUCT_ROUTER.get('/construcciones',   ConstruccionProductoController.allConstruccion);
+PRODUCT_ROUTER.post('/construccion',    ConstruccionProductoController.saveConstruccion);
+PRODUCT_ROUTER.put('/construccion/:id', ConstruccionProductoController.modifyConstruccion);
 
 
 // Rutas de productos generales.
