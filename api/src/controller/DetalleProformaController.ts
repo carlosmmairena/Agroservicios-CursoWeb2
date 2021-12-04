bc8deeb0ef24935e3b97610d355ae49fa5432937
Carlos Mairena <44934161+carlosmmairena@users.noreply.github.com>
2021-12-04 03:49:56 -0600

    Carlosmmairena/endpoints proformas (#11)
    * Se agregan las rutas para Crear y Mostrar proformas
    
    * Se agrega el endpoint para modificar una proforma
    
    * Se agrega el endpoint para eliminar una proforma
    
    * Se agregan los endpoints para agregar detalles y eliminar un detalle de proformas
    
    * Se agrega el endpoint para mostrar una proforma y sus detalles
    
    * Se actualiza el endpoint para crear y actualizar una proforma
    
    * Se crea endpoint para marcar como cancelada una proforma

---
+++ api/src/controller/DetalleProformaController.ts
@@ -0,0 +1,120 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { isArray, isNullOrUndefined } from "util";
+import { DetalleProforma } from "../entity/DetalleProforma";
+import { Producto } from "../entity/Producto";
+import { Proforma } from "../entity/Proforma";
+
+export class DetalleProformController {
+
+    /**
+     * Agrega una lista de detalles a una proforma o a la lista de deseos.
+     * 
+     * @param request 
+     * @param response 
+     */
+    static addDetalles = async (request: Request, response: Response) => {
+        try {
+            const { id }        = request.params;
+            const { productos } = request.body;
+
+            if (isNullOrUndefined(productos)) {
+                return response.status(422).json({ message: 'productos es requerido.' });
+            }
+
+            if (!isArray(productos)) {
+                return response.status(422).json({ message: 'productos debe ser un array.' });
+            }
+
+            if (productos.length < 1) {
+                return response.status(422).json({ message: 'productos no puede ser vacío' });
+            }
+
+
+            // Busca la proforma a editar
+            const proformaRepository = getRepository(Proforma);
+            const proformaToEdit     = await proformaRepository.findOne(id, { where: { estado: true } });
+
+            if (isNullOrUndefined(proformaToEdit)) {
+                return response.status(404).json({ message: `No se encuentra una proforma con ID ${id}.` });
+            }
+
+            // Busca los productos a guardar, y crea el detalle de cada uno.
+            const productoRepository = getRepository(Producto);
+
+            const detalles = productos.map( async (value) => {
+                const productLocated = await productoRepository.findOne(value.id);
+
+                if (!isNullOrUndefined(value)) {
+                    return DetalleProforma.create(productLocated, proformaToEdit, value.cantidadComprar);
+                }
+            })
+
+
+            // Obtenemos las promesas.
+            const detallesToSave = await Promise.all<DetalleProforma>(detalles).then((detalles) => {
+                return detalles;
+            }).catch((error) => {
+                console.error(error);
+                return undefined;
+            });
+
+
+            // Se guarda cada uno de los detalles
+            const detalleRepository = getRepository(DetalleProforma);
+            detallesToSave.forEach( async (detalle) => {
+                await detalleRepository.save(detalle);
+            });
+
+            return response.status(201).json({ message: 'Detalles de proforma han sido agregados.' });
+
+        } catch (error) {
+            const messages = {
+                message: 'Algo ha salido mal...',
+                error: error
+            };
+
+            console.error(error);
+            return response.status(503).json(messages);
+        }
+    }
+
+
+    /**
+     * Remueve un detalle de una proforma.
+     * 
+     * @param request 
+     * @param response 
+     */
+    static removeDetalle = async (request: Request, response: Response) => {
+        try {
+
+            const { id } = request.params;
+
+            // Busca el detalle a eliminar
+            const detalleRepository = getRepository(DetalleProforma);
+            const detalleToRemove   = await detalleRepository.findOne(id);
+
+            if (isNullOrUndefined(detalleToRemove)) {
+                return response.status(404).json({ message: `No se encuentra un detalle con ID ${id}.` });
+            }
+
+            await detalleRepository.remove(detalleToRemove);
+
+            return response.status(201).json({ message: 'Detalle de proforma ha sido eliminado.', detalle: detalleToRemove });
+
+        } catch (error) {
+            const messages = {
+                message: 'Algo ha salido mal...',
+                error: error
+            };
+
+            console.error(error);
+            return response.status(503).json(messages);
+        }
+    }
+
+    static updateDetalle = async (request: Request, response: Response) => {
+    }
+
+}
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,12 +3,14 @@ import { checkJWT } from "../middleware/JwtMiddleware";
 import USER_ROUTER from "./UserRouter";
 import AUTH_ROUTER from "./AuthRouter";
 import PRODUCT_ROUTER from "./ProductRouter";
+import PROFORMA_ROUTER from "./ProformaRouter";
 
 
 const ROUTER = Router();
 
 ROUTER.use('/usuario', USER_ROUTER);
 ROUTER.use('/producto', [checkJWT], PRODUCT_ROUTER);
+ROUTER.use('/proforma', [checkJWT], PROFORMA_ROUTER);
 ROUTER.use('/auth', AUTH_ROUTER);
 
 export default ROUTER;
