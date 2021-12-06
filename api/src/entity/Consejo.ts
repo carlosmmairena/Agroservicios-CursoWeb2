7729b73ce69e3c0cf0f84a2aa41e036c0f9c2fe5
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-12-06 16:45:27 -0600

    Nuevo endpoint, crud consejos

--- api/src/entity/Consejo.ts
+++ api/src/entity/Consejo.ts
@@ -1,5 +1,6 @@
 import { IsBoolean, IsNotEmpty, MaxLength, MinLength } from "class-validator";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { isNullOrUndefined } from "util";
 import { Usuario } from "./Usuario";
 
 @Entity("Consejos")
@@ -29,5 +30,32 @@ export class Consejo {
     @ManyToOne(() => Usuario, usuario => usuario.consejos)
     @JoinColumn({  name: 'idUsuario', referencedColumnName: 'id' })
     usuario: Usuario;
+
+    static checkData(data) : any {
+
+        let dataChecked = { hasErrors: false, errors: {}, message: "Nada para cambiar" };
+
+        if (isNullOrUndefined(data.titulo)) {
+            dataChecked.errors['titulo'] = 'titulo es requerido';
+            dataChecked.hasErrors = true;
+        }
+
+        if (isNullOrUndefined(data.foto)) {
+            dataChecked.errors['foto'] = 'foto es requerido';
+            dataChecked.hasErrors            = true;
+        }
+
+        if (isNullOrUndefined(data.estado)) {
+            dataChecked.errors['estado'] = 'estado es requerido';
+            dataChecked.hasErrors            = true;
+        }
+
+        if (isNullOrUndefined(data.idUsuario)) {
+            dataChecked.errors['idUsuario'] = 'idUsuario es requerido';
+            dataChecked.hasErrors            = true;
+        }
+
+        return dataChecked;
+    }
 
 }
