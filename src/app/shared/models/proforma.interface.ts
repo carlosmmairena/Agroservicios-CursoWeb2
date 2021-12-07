397bdd391395340fb9d9d4394fc8ab4d0293eeaa
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-12-07 07:14:07 -0600

    Se crea modelo interface para proformas

---
+++ src/app/shared/models/proforma.interface.ts
@@ -0,0 +1,18 @@
+
+export interface ProformasResponse {
+
+    message:   string;
+    proformas: Proforma[]
+
+}
+
+
+export interface Proforma {
+    id:                  number;
+    fechaEmision:        Date;
+    formaPago:           string;
+    estado:              boolean;
+    canceled:            boolean;
+    porcentajeDescuento: number;
+    cliente:             Object;
+}
